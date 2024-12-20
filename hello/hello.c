8e7bf83472e9282e53da8b3a4df080f63903bd4a
leo1000-corso <mail.lavoro1000@gmail.com>
2024-12-20 16:08:34 +0000

    hello.c updated

--- hello/hello.c
+++ hello/hello.c
@@ -6,7 +6,20 @@ int main()
     //print the message "Hello world!!!" to the console
     // \n is the shortcut for new linw
     printf("Hello world!!!\n");
-    /* return 0: indicated that program is terminated succesfully*/
+
+    //variable of 32 chars
+    char name[32];
+
+    // ask for aa name
+    printf("Enter a name:\n");
+
+    // read the input of the user
+    scanf("%s", name);
+
+    //print the message wtih the name entered
+    printf("\nHello\t%s\n\n\n", name);
+    
+    /* return 0: indicated that program is terminated succesfully */
 
     return 0;
 }
