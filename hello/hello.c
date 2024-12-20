63a28c81fcfba2c21ce95726607ea2312c7c348d
leo1000-corso <mail.lavoro1000@gmail.com>
2024-12-20 15:44:33 +0000

    created hello.c

---
+++ hello/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// mian function - the entry point
+// the return value of the main is an integer
+int main()
+{
+    //print the message "Hello world!!!" to the console
+    // \n is the shortcut for new linw
+    printf("Hello world!!!\n");
+    /* return 0: indicated that program is terminated succesfully*/
+
+    return 0;
+}
